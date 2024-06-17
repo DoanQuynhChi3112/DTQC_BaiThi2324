4d08f2a7ddf3815c3456ae2c9fcc59e32f3e2a95
DoanQuynhChi3112 <doanthiquynhchi3112@gmail.com>
2024-06-17 12:43:44 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DTQC_BaiThi2324.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
